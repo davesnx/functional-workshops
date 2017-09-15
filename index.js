fc7a25c08d6ef7babfdb38f6035083eba911ab88
David Sancho <dsnxmoreno@gmail.com>
2017-09-15 09:13:39 +0200

    Initial commit :star:

---
+++ index.js
@@ -0,0 +1,28 @@
+const R = require('ramda')
+
+/*
+
+type alias Pokemon = {
+  id: Number,
+  name: String,
+  type: PokemonTypes,
+  position: [Number, Number]
+}
+
+type PokemonTypes = Flying | Poison | Water | Ice | Electric | Normal
+
+*/
+
+const findPikachuPosition = () => {}
+const closestPokemonName = () => {}
+const distanceBetweenPokemons = () => {}
+const validateEnemiesMap = () => {}
+const createEnemies = () => {}
+
+module.exports = {
+  findPikachuPosition: findPikachuPosition,
+  closestPokemonName: closestPokemonName,
+  distanceBetweenPokemons: distanceBetweenPokemons,
+  validateEnemiesMap: validateEnemiesMap,
+  createEnemies: createEnemies
+}
---
+++ test.js
@@ -0,0 +1,134 @@
+const {
+  findPikachuPosition,
+  closestPokemonName,
+  distanceBetweenPokemons,
+  validateEnemiesMap,
+  createEnemies
+} = require('./index')
+
+const Pokemons = [
+  {
+    id: 1,
+    type: 'Flying',
+    name: 'Pidgey',
+    position: [27, 90]
+  },
+  {
+    id: 2,
+    type: 'Poison',
+    name: 'Nidoran',
+    position: [66, 12]
+  },
+  {
+    id: 3,
+    type: 'Poison',
+    name: 'Bell sprout',
+    position: [99, 99]
+  },
+  {
+    id: 4,
+    type: 'Normal',
+    name: 'Mewtwo',
+    position: [24, 12]
+  },
+  {
+    id: 5,
+    type: 'Water',
+    name: 'Magikarp',
+    position: [4, 3]
+  },
+  {
+    id: 6,
+    type: 'Water',
+    name: 'Gyarados',
+    position: [0, 0]
+  },
+  {
+    id: 7,
+    type: 'Normal',
+    name: 'Rattata',
+    position: [5, 30]
+  },
+  {
+    id: 8,
+    type: 'Normal',
+    name: 'Rattata',
+    position: [80, 44]
+  },
+  {
+    id: 9,
+    type: 'Normal',
+    name: 'Zubat',
+    position: [81, 46]
+  },
+  {
+    id: 10,
+    type: 'Ice',
+    name: 'Lapras',
+    position: [20, 94]
+  },
+  {
+    id: 11,
+    type: 'Electric',
+    name: 'Pikachu',
+    position: [123, 55]
+  }
+]
+
+describe('Pikachu Test Suite', () => {
+  it(`should find Pikachu's position`, () => {
+    const pikachuPosition = [123, 55]
+    expect(findPikachuPosition(Pokemons)).toEqual(pikachuPosition)
+  })
+
+  it('should get the distance between 2 pokemons', () => {
+    const Gyarados = Pokemons[4]
+    const Magikarp = Pokemons[5]
+
+    expect(distanceBetweenPokemons(Magikarp, Gyarados)).toEqual(5)
+  })
+
+  it('should find the closest Pokemon given a position', () => {
+    const position = [4, 29]
+    const pokemonName = 'Rattata'
+    const firstPokemon = Pokemons[0].name
+
+    expect(closestPokemonName(Pokemons, position)).toEqual(pokemonName)
+    expect(closestPokemonName(Pokemons, position)).not.toEqual(firstPokemon)
+  })
+
+  describe('Pokemon enemies', () => {
+    const enemiesMap = {
+      Normal: ['Flying', 'Poison'],
+      Water: ['Poison'],
+      Flying: ['Normal', 'Ice'],
+      Poison: ['Normal', 'Water'],
+      Ice: ['Flying', 'Water'],
+      Electric: []
+    }
+
+    it('validates that enemiesMap make sense', () => {
+      expect(validateEnemiesMap(enemiesMap)).toBeTruthy()
+    })
+
+    it('should generate the enemies of each Pokemon', () => {
+      const enemies = createEnemies(Pokemons, enemiesMap)
+
+      const mockEnemies = {
+        1: [4, 7, 8, 9],
+        2: [4, 5, 6, 7, 8, 9],
+        3: [4, 5, 6, 7, 8, 9],
+        4: [1, 2, 3],
+        5: [2, 3],
+        6: [2, 3],
+        7: [1, 2, 3],
+        8: [1, 2, 3],
+        9: [1, 2, 3],
+        10: [1, 5, 6],
+        11: []
+      }
+
+      expect(enemies).toEqual(mockEnemies)
+    })
+  })
+})
