77ce995ac3b70295f476f40118cdef400c025491
Daniel Ostapenko <danielostapenko@gmail.com>
2017-09-15 13:14:07 +0200

    denieler solution before fp

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var R = require('ramda')
+
 /*
 
 type alias Pokemon = {
@@ -11,11 +13,68 @@ type PokemonTypes = Flying | Poison | Water | Ice | Electric | Normal
 
 */
 
-const findPikachuPosition = () => {}
-const closestPokemonName = () => {}
-const distanceBetweenPokemons = () => {}
-const validateEnemiesMap = () => {}
-const createEnemies = () => {}
+const dist = ([x1, y1], [x2, y2]) => {
+  return Math.sqrt(
+    Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)
+  )
+}
+
+const isPikachu = p => R.prop('name', p) === 'Pikachu'
+
+const findPikachuPosition = (pokemons) => {
+  return R.pipe(
+    R.filter(isPikachu),
+    R.head,
+    R.prop('position')
+  )(pokemons)
+}
+
+const closestPokemonName = (pokemons, [x, y]) => {
+  return pokemons.map(p => (
+    {
+      name: p.name,
+      distance: dist(p.position, [x, y])
+    }
+  )).sort((a, b) => {
+    return a.distance - b.distance
+  })[0].name
+}
+
+const distanceBetweenPokemons = (firstPokemon, secondPokemon) => {
+  var [x1, y1] = firstPokemon.position
+  var [x2, y2] = secondPokemon.position
+  
+  return dist([x1, y1], [x2, y2])
+}
+
+const validateEnemiesMap = (enemiesMap) => {
+  return Object.keys(enemiesMap)
+  .map(enemy => 
+    enemiesMap[enemy]
+    .find(l => 
+      enemiesMap[l].indexOf(enemy) <= -1
+    )
+  )
+  .every(x => !x)
+}
+
+const createEnemies = (pokemons, enemiesMap) => {
+  return pokemons
+  .sort(x => x.id)
+  .reduce((acc, p) => {
+    const enemies = enemiesMap[p.type]
+    .reduce((accum, enemyType) => {
+      const enemiesIdsArray = pokemons
+        .filter(x => x.type === enemyType)
+        .map(p => p.id)
+      accum = accum.concat(enemiesIdsArray ? enemiesIdsArray : [])
+      return accum
+    }, [])
+
+    acc[p.id] = enemies.sort((a, b) => a - b)
+    return acc
+  }, {})
+}
 
 module.exports = {
   findPikachuPosition: findPikachuPosition,
