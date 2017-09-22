9b5533b3041b58a0eadf47f0ece1684b8769a26d
Daniel Ostapenko <danielostapenko@gmail.com>
2017-09-22 12:00:03 +0200

    functional solution for pokemons tests

--- index.js
+++ index.js
@@ -19,25 +19,27 @@ const dist = ([x1, y1], [x2, y2]) => {
   )
 }
 
-const isPikachu = p => R.prop('name', p) === 'Pikachu'
-
+const isPikachu = R.propEq('name', 'Pikachu')
+const findPikachu = R.pipe(
+  R.filter(isPikachu), 
+  R.head
+)
+const takePokemonPosition = R.prop('position')
 const findPikachuPosition = (pokemons) => {
   return R.pipe(
-    R.filter(isPikachu),
-    R.head,
-    R.prop('position')
+    findPikachu,
+    takePokemonPosition
   )(pokemons)
 }
 
+const pokemonDistanceToPoint = ([x, y]) => p => dist(R.prop('position', p), [x, y])
+const defaultPokemon = { position: [Infinity, Infinity] }
+
 const closestPokemonName = (pokemons, [x, y]) => {
-  return pokemons.map(p => (
-    {
-      name: p.name,
-      distance: dist(p.position, [x, y])
-    }
-  )).sort((a, b) => {
-    return a.distance - b.distance
-  })[0].name
+  return R.pipe(
+    R.reduce(R.minBy(pokemonDistanceToPoint([x, y])), defaultPokemon),
+    R.prop('name')
+  )(pokemons)
 }
 
 const distanceBetweenPokemons = (firstPokemon, secondPokemon) => {
@@ -47,33 +49,32 @@ const distanceBetweenPokemons = (firstPokemon, secondPokemon) => {
   return dist([x1, y1], [x2, y2])
 }
 
+const isEnemyExist = enemiesMap => x => y => R.contains(x, R.prop(y)(enemiesMap))
 const validateEnemiesMap = (enemiesMap) => {
-  return Object.keys(enemiesMap)
-  .map(enemy => 
-    enemiesMap[enemy]
-    .find(l => 
-      enemiesMap[l].indexOf(enemy) <= -1
+  return R.pipe(
+    R.toPairs,
+    R.all(([x, list]) => 
+      R.all(y => isEnemyExist(enemiesMap, x, y), list)
     )
-  )
-  .every(x => !x)
+  )(enemiesMap)
 }
 
-const createEnemies = (pokemons, enemiesMap) => {
-  return pokemons
-  .sort(x => x.id)
-  .reduce((acc, p) => {
-    const enemies = enemiesMap[p.type]
-    .reduce((accum, enemyType) => {
-      const enemiesIdsArray = pokemons
-        .filter(x => x.type === enemyType)
-        .map(p => p.id)
-      accum = accum.concat(enemiesIdsArray ? enemiesIdsArray : [])
-      return accum
-    }, [])
+const takePokemonType = R.prop('type')
+const takePokemonId = R.prop('id')
+const takePokemonIds = R.map(takePokemonId)
+const takeTypeEnemies = R.curry((enemiesMap, type) => R.prop(type, enemiesMap))
+const takeIdsForTypes = R.curry((pokemons, types) => R.pipe(
+  R.innerJoin((pokemon, type) => pokemon.type === type),
+  R.map(p => takePokemonId(p))
+)(pokemons, types))
 
-    acc[p.id] = enemies.sort((a, b) => a - b)
-    return acc
-  }, {})
+const createEnemies = (pokemons, enemiesMap) => {
+  return R.pipe(
+    R.map(takePokemonType),
+    R.map(takeTypeEnemies(enemiesMap)),
+    R.map(takeIdsForTypes(pokemons)),
+    R.zipObj(takePokemonIds(pokemons))
+  )(pokemons)
 }
 
 module.exports = {
